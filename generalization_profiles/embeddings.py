af977aedcbd2f5517cf595e7b275bda5334d50fc
lamort <lamort@google.com>
2025-11-22 17:35:13 +0100

    improve embedding caching/handling and change python

--- generalization_profiles/embeddings.py
+++ generalization_profiles/embeddings.py
@@ -1,5 +1,6 @@
 import shelve
 from dataclasses import dataclass
+from functools import cached_property
 from pathlib import Path
 
 from fire import Fire
@@ -9,6 +10,10 @@ from sentence_transformers import SentenceTransformer
 from tqdm.auto import tqdm
 
 from generalization_profiles.pythia_model import PythiaModel
+
+
+CACHE_LOCATION = Path("cache/embeddings")
+ALIBABA_MODEL = "Alibaba-NLP/gte-multilingual-base"
 
 
 @dataclass
@@ -79,15 +84,46 @@ def compute_and_cache_embeddings(
     return embeddings
 
 
-def main(model='Alibaba-NLP/gte-multilingual-base'):
-    data = load_and_detokenize_pile_subset()
-    compute_and_cache_embeddings(
-        encoder=SentenceTransformer(model, trust_remote_code=True),
-        cache_location=f"cache/embeddings/{model}",
-        data=data,
+class Embeddings:
+    # (num samples, embedding dimension)
+    values: np.ndarray
+
+    # (num samples,)
+    seq_idx: np.ndarray
+
+    def __init__(self, values: np.ndarray, seq_idx: np.ndarray):
+        self.values = values
+        self.seq_idx = seq_idx
+        self._id_map = {seq_index: i for i, seq_index in enumerate(self.seq_idx)}
+        self._map_idx = np.vectorize(self._id_map.__getitem__)
+
+    def __getitem__(self, item):
+        # Accepts `seq_idx` values or arrays.
+        return self.values[self._map_idx(item)]
+
+
+def load_from_cache(model: str) -> Embeddings:
+    cache_location = (CACHE_LOCATION / "numpy" / model).with_suffix(".npz")
+    data = np.load(cache_location)
+    return Embeddings(
+        values=data["values"],
+        seq_idx=data["seq_idx"],
     )
 
 
-if __name__ == '__main__':
+def main(model=ALIBABA_MODEL):
+    data = load_and_detokenize_pile_subset()
+    embeddings = compute_and_cache_embeddings(
+        encoder=SentenceTransformer(model, trust_remote_code=True),
+        cache_location=CACHE_LOCATION / model,
+        data=data,
+    )
+
+    numpy_save_path = Path(CACHE_LOCATION / "numpy" / model)
+    numpy_save_path.parent.mkdir(parents=True, exist_ok=True)
+    seq_idx, values = zip(*[(e.seq_idx, e.value) for e in embeddings])
+    np.savez(numpy_save_path, seq_idx=seq_idx, values=values)
+
+
+if __name__ == "__main__":
     Fire(main)
-    
