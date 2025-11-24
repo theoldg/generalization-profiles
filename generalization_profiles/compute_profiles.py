ec99ca14f4dd319ac08089a0df2b86834339dfd4
lamort <lamort@google.com>
2025-11-24 13:52:43 +0100

    load surprisals

--- generalization_profiles/compute_profiles.py
+++ generalization_profiles/compute_profiles.py
@@ -1,16 +1,21 @@
 from dataclasses import dataclass
 
 import numpy as np
+import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from tqdm.auto import tqdm
+import datasets
 
 from generalization_profiles.embeddings import Embeddings
 
 
 @dataclass
 class Neighbors:
-    # (num seeds, top k)
+    # (num seeds,)
     seq_idx: np.ndarray
+
+    # (num seeds, top k)
+    neighbor_idx: np.ndarray
 
     # (num seeds, top k)
     cosine_similarity: np.ndarray
@@ -30,7 +35,8 @@ class NeighborFinder:
         neighbor_idx = self.neighbor_set_ids[top_args]
         similarities = np.take_along_axis(similarities, top_args, axis=1)
         return Neighbors(
-            seq_idx=neighbor_idx,
+            seq_idx=seeds,
+            neighbor_idx=neighbor_idx,
             cosine_similarity=similarities,
         )
 
@@ -54,15 +60,63 @@ def find_all_neighbors(
         id_chunk = embeddings.seq_idx[i : i + batch_size]
         chunks.append(finder.find_neighbors(id_chunk, top_k))
 
-    print(
-        f"""
-        {chunks[0].seq_idx.shape = }
-        {chunks[0].cosine_similarity.shape = }            
-    """
-    )
     return Neighbors(
-        seq_idx=np.concatenate([n.seq_idx for n in chunks], axis=0),
+        seq_idx=np.concatenate([n.seq_idx for n in chunks]),
+        neighbor_idx=np.concatenate([n.neighbor_idx for n in chunks], axis=0),
         cosine_similarity=np.concatenate(
             [n.cosine_similarity for n in chunks], axis=0
         ),
     )
+
+
+@dataclass
+class Surprisals:
+    """For a fixed model, contains the surpisal of sample n at step m."""
+
+    # (n steps,)
+    step: np.ndarray
+
+    # (n samples,)
+    seq_idx: np.ndarray
+
+    # (n steps, n samples)
+    values: np.ndarray
+
+
+def _to_dataframe(data: datasets.Dataset) -> pd.DataFrame:
+    # Not sure how this works but it's way faster than `pd.DataFrame(data)`
+    return data.with_format('pandas')[:]  # type: ignore
+
+
+def _load_surprisals_single(data: datasets.Dataset) -> Surprisals:
+    df = _to_dataframe(data)
+    df = df.sort_values(by=['step', 'seq_idx'])
+    steps = []
+    values = []
+    seq_idx: np.ndarray | None = None
+    for step, values_at_step in df.groupby('step', sort=False):
+        steps.append(step)
+        values.append(values_at_step['sup_seq'].values)
+
+        # Make sure the seq_idx are the same for every step
+        current_seq_idx = np.array(values_at_step['seq_idx'].values)
+        if seq_idx is None:
+            seq_idx = current_seq_idx
+        else:
+            assert (seq_idx == current_seq_idx).all()
+            
+    assert seq_idx is not None
+    
+    return Surprisals(
+        step=np.array(steps),
+        seq_idx=seq_idx,
+        values=np.stack(values)
+    )
+    
+    
+def load_surprisals() -> dict[str, Surprisals]:
+    dataset = datasets.load_dataset('pietrolesci/pythia-deduped-stats')
+    return {
+        model_name: _load_surprisals_single(data)  # type: ignore
+        for model_name, data in dataset.items()  # type: ignore
+    }
--- generalization_profiles/pythia_model.py
+++ generalization_profiles/pythia_model.py
@@ -3,9 +3,15 @@ from typing import Self
 
 from transformers import AutoTokenizer, GPTNeoXForCausalLM
 
-MODEL_VARIANTS: set[str] = {
-    '70m-deduped',
-}
+MODEL_VARIANTS: list[str] = [
+    "70m",
+    "160m",
+    "410m",
+    "1.4b",
+    "2.8b",
+    "6.9b",
+    "12b",
+]
 
 # Valid revisions are 0, 1000, 2000, ..., 143000 for all models.
 VALID_REVISIONS: set[int] = {step for step in range(0, 144000, 1000)}
@@ -24,26 +30,26 @@ class PythiaModel:
         cls,
         variant: str,
         revision: int,
-        cache_dir: str | Path = 'cache/pythia',
+        cache_dir: str | Path = "cache/pythia",
     ) -> Self:
         cache_dir = Path(cache_dir)
         if variant not in MODEL_VARIANTS:
-            raise ValueError(f'Invalid model variant: {variant}')
+            raise ValueError(f"Invalid model variant: {variant}")
         if revision not in VALID_REVISIONS:
             raise ValueError(
-                f'Invalid revision: {revision}. '
-                '(Should be a number from 0 to 143000 divisible by 1000).'
+                f"Invalid revision: {revision}. "
+                "(Should be a number from 0 to 143000 divisible by 1000)."
             )
         model = GPTNeoXForCausalLM.from_pretrained(
-            f'EleutherAI/pythia-{variant}',
-            revision=f'step{revision}',
+            f"EleutherAI/pythia-{variant}",
+            revision=f"step{revision}",
             cache_dir=cache_dir / variant / str(revision),
         )
         tokenizer = AutoTokenizer.from_pretrained(
-            f'EleutherAI/pythia-{variant}',
-            revision=f'step{revision}',
+            f"EleutherAI/pythia-{variant}",
+            revision=f"step{revision}",
             cache_dir=cache_dir / variant / str(revision),
         )
         # See huggingface.co/EleutherAI/pythia-6.9b/raw/main/tokenizer.json
-        tokenizer.pad_token = '<|padding|>'
+        tokenizer.pad_token = "<|padding|>"
         return cls(model=model, tokenizer=tokenizer)
